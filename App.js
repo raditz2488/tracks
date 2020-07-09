68018ad95c9f93f46e0b70d2c7be9accc58fe699
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-09 19:06:18 +0530

    Imported all the screens into App.js

--- App.js
+++ App.js
@@ -1,21 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import SignupScreen from './src/screens/SignupScreen';
+import SigninScreen from './src/screens/SigninScreen';
+import AccountScreen from './src/screens/AccountScreen';
+import TrackListScreen from './src/screens/TrackListScreen';
+import TrackDetailScreen from './src/screens/TrackDetailScreen';
+import TrackCreateScreen from './src/screens/TrackCreateScreen';
