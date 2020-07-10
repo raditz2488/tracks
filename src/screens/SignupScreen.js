264014a00c466cc05b2b9a2382c47529961e7bcc
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-10 08:34:48 +0530

    Imported Button, Text and Header from the react-native-elements into SignupScreen.

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
-import { Button } from 'react-native-elements';
+import { View, StyleSheet } from 'react-native';
+import { Button, Text, Header  } from 'react-native-elements';
 
 const SignupScreen = ( { navigation } ) => {
     return <>
