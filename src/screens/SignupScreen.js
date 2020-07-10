0181e41138423b9942c0605c458f46bc215f4fd5
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-10 08:38:02 +0530

    Corrected the import from

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Button, Text, Header  } from 'react-native-elements';
+import { Button, Text, Input  } from 'react-native-elements';
 
 const SignupScreen = ( { navigation } ) => {
     return <>
-        <Text style={{ fontSize: 48 }}>SignupScreen</Text>
+        <Text h3>SignupScreen</Text>
         <Button title={"Show signin"} onPress={() => navigation.navigate('Signin')}/>
         <Button title="Show main flow" onPress={() => navigation.navigate('mainFlow')}/>
     </>
