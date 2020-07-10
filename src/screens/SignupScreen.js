01494222d36820f1f7f1051b172c06cea59735cf
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-10 08:30:59 +0530

    Added button to SignupScreen to navigate to main flow.

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -6,6 +6,7 @@ const SignupScreen = ( { navigation } ) => {
     return <>
         <Text style={{ fontSize: 48 }}>SignupScreen</Text>
         <Button title={"Show signin"} onPress={() => navigation.navigate('Signin')}/>
+        <Button title="Show main flow" onPress={() => navigation.navigate('mainFlow')}/>
     </>
 }
 
