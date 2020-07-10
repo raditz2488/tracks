72ca57a6764f496855c3b808564c2ddc6cc87f99
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-10 08:46:05 +0530

    Imported Spacer into the SignupScreen.

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Button, Text, Input  } from 'react-native-elements';
+import Spacer from '../components/Spacer';
 
 const SignupScreen = ( { navigation } ) => {
     return <>
