09160b2198221f7ae1db79c8f0630207fe2b95e8
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-10 08:28:42 +0530

    Added buttons to navigate between SignupScreen and SigninScreen.

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 
-const SigninScreen = () => {
-    return <Text style={{ fontSize: 48 }}>SigninScreen</Text>
+const SigninScreen = ( { navigation } ) => {
+    return <>
+        <Text style={{ fontSize: 48 }}>SigninScreen</Text>
+        <Button title="Show signup" onPress={() => navigation.navigate('Signup') }/>
+    </>
 }
 
 const styles = StyleSheet.create({});
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 
-const SignupScreen = () => {
-    return <Text style={{ fontSize: 48 }}>SignupScreen</Text>
+const SignupScreen = ( { navigation } ) => {
+    return <>
+        <Text style={{ fontSize: 48 }}>SignupScreen</Text>
+        <Button title={"Show signin"} onPress={() => navigation.navigate('Signin')}/>
+    </>
 }
 
 const styles = StyleSheet.create({});
