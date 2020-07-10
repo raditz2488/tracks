218808441771837069cf23d28ece9b2fbfd2e9f1
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-10 08:51:57 +0530

    Updated the Spacers View style to container as earlier margin was a typo. Also the margin in the container style is updated to 15.

--- src/components/Spacer.js
+++ src/components/Spacer.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
 const Spacer = ({ children }) => {
-    return <View style={styles.margin}>
+    return <View style={styles.container}>
         {children}
     </View>
 }
 
 const styles = StyleSheet.create({
     container: {
-        margin: 10
+        margin: 15
     }
 });
 
