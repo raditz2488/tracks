ba616d34910a0fa801be4b9247a63690b0a68fda
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-10 15:04:50 +0530

    The userReducer hook is now associated with the Provider in the createDataContext.

--- src/contexts/createDataContext.js
+++ src/contexts/createDataContext.js
@@ -1,11 +1,11 @@
 import React, { useReducer } from 'react';
 
 export default (reducer, actions, initialValue) => {
-    const [state, dispatch] = useReducer(reducer, initialValue)
-
     const Context = React.createContext();
 
     const Provider = ({ children }) => {
+        const [state, dispatch] = useReducer(reducer, initialValue)
+
         const dispatchActions = {};
 
         for (key in actions) {
