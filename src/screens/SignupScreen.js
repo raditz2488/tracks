1ea8f8aca44029230069b34bca8f31b795302166
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-13 20:01:01 +0530

    Added code to useContext to get state and signup from the AuthContext in the SignupScreen.

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -2,9 +2,10 @@ import React, { useContext } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Button, Text, Input  } from 'react-native-elements';
 import Spacer from '../components/Spacer';
-import { Context as AuthContext } from '../contexts/AuthContext';
+import { Context as AuthContext, Context } from '../contexts/AuthContext';
 
 const SignupScreen = ( { navigation } ) => {
+    const { state, signup } = useContext(AuthContext);
     return <View>
         <Spacer>
         <Text h3>Sign Up for Tracker</Text>
