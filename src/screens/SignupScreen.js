eef97c56e8ec61ae99ac2c774e0f0dbddff67a47
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-13 20:09:40 +0530

    Import useState in the SignupScreen

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Button, Text, Input  } from 'react-native-elements';
 import Spacer from '../components/Spacer';
