924c2606fb39c3eea576a1b0c881b513844b9045
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-14 22:19:16 +0530

    Exported navigate function which uses navigator to dispatch navigate to route with params.

--- src/navigatorRef.js
+++ src/navigatorRef.js
@@ -5,3 +5,12 @@ let navigator;
 export const setNavigator = (nav) => {
     navigator = nav;
 }
+
+export const navigate = (routeName, params) => {
+    navigator.dispatch(
+        NavigationActions.navigate({
+            routeName,
+            params
+        })
+    );
+};
