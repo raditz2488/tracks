45a5f29a0f08ff8ee584481a6ca6f2fce697b02a
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-15 10:31:43 +0530

    Imported AuthForm into the SignupScreen.

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -3,6 +3,7 @@ import { View, StyleSheet } from 'react-native';
 import { Button, Text, Input  } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 import { Context as AuthContext, Context } from '../contexts/AuthContext';
+import AuthForm from '../components/AuthForm';
 
 const SignupScreen = ( { navigation } ) => {
      
