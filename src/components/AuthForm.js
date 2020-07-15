3c76e14c6c909c0af4bbc1cb21d649ea0cbb889c
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-15 10:02:45 +0530

    Extracted form component from the SignupScreen into AuthForm.

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -2,3 +2,35 @@ import { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Button, Text, Input  } from 'react-native-elements';
 import Spacer from '../components/Spacer';
+
+const AuthForm = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    
+    return (
+        <>
+            <Spacer>
+            <Text h3>Sign Up for Tracker</Text>
+            </Spacer>
+            <Input label='Email'
+                value={email}
+                onChangeText={ setEmail }
+                autoCorrect={false}
+                autoCapitalize='none'
+            />
+            <Spacer/>
+            <Input label='Password'
+                value={password}
+                onChangeText={ setPassword }
+                autoCorrect={false}
+                autoCapitalize='none'
+                secureTextEntry
+            />
+            {state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null} 
+            <Spacer>
+            <Button title={"Signup"} onPress={()=> signup(email, password)}/>
+            </Spacer>
+        </>
+    );
+}
+
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -5,33 +5,12 @@ import Spacer from '../components/Spacer';
 import { Context as AuthContext, Context } from '../contexts/AuthContext';
 
 const SignupScreen = ( { navigation } ) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState(''); 
+     
 
     const { state, signup } = useContext(AuthContext);
 
     return <View>
-        <Spacer>
-        <Text h3>Sign Up for Tracker</Text>
-        </Spacer>
-        <Input label='Email'
-            value={email}
-            onChangeText={ setEmail }
-            autoCorrect={false}
-            autoCapitalize='none'
-        />
-        <Spacer/>
-        <Input label='Password'
-            value={password}
-            onChangeText={ setPassword }
-            autoCorrect={false}
-            autoCapitalize='none'
-            secureTextEntry
-        />
-        {state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null} 
-        <Spacer>
-        <Button title={"Signup"} onPress={()=> signup(email, password)}/>
-        </Spacer>
+        
     </View>
 }
 
