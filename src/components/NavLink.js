906c1589d8d98a52d3127124545bda197537d55f
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-15 10:50:38 +0530

    Implemented NavLinkand exported default.

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -2,3 +2,14 @@ import React from 'react';
 import { Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { withNavigation } from 'react-navigation';
 
+const NavLink = ({ navigation, text, routeName }) => {
+    return (
+        <TouchableOpacity onPress={ navigation.navigate(routeName) }>
+            <Text>
+                {text}
+            </Text>
+        </TouchableOpacity>
+    );
+}
+
+export default NavLink;
