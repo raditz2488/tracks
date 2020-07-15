8c5eb842ce0a38e49ec892d253a2a40bab2caaf9
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-15 11:05:54 +0530

    Reused the AuthForm and NavLink in the SigninScreen.

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { Button } from 'react-native-elements';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
 
 const SigninScreen = ( { navigation } ) => {
-    return <>
-        <Text style={{ fontSize: 48 }}>SigninScreen</Text>
-        <Button title="Show signup" onPress={() => navigation.navigate('Signup') }/>
-    </>
+    return <View>
+        <AuthForm
+            headerText="Sign In for tracks"
+            errorMessage=""
+            buttonText="Sign In"
+            onSubmit={(email, password) => {}}
+        />
+        <NavLink text="Create an account! Sign Up" routeName='Signup'/>
+    </View>
 }
 
 const styles = StyleSheet.create({});
