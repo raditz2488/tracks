3ef8305dd7cabba814a6e6a2989fbf2b328b634b
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-15 10:28:32 +0530

    Added params named headerText, errorMessage, buttonText and onSubmit to AuthForm. Used them in the children components.

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -3,14 +3,14 @@ import { StyleSheet } from 'react-native';
 import { Button, Text, Input  } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 
-const AuthForm = () => {
+const AuthForm = ({ headerText, errorMessage, buttonText, onSubmit }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     return (
         <>
             <Spacer>
-            <Text h3>Sign Up for Tracker</Text>
+            <Text h3>{headerText}</Text>
             </Spacer>
             <Input label='Email'
                 value={email}
@@ -26,9 +26,9 @@ const AuthForm = () => {
                 autoCapitalize='none'
                 secureTextEntry
             />
-            {state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null} 
+            {errorMessage ? <Text style={styles.errorMessage}>{errorMessage}</Text> : null} 
             <Spacer>
-            <Button title={"Signup"} onPress={()=> signup(email, password)}/>
+            <Button title={buttonText} onPress={()=> onSubmit(email, password)}/>
             </Spacer>
         </>
     );
