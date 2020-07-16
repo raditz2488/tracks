e3ef59b5f8ee991b6efa397111b9c87ccb940e97
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-16 11:07:59 +0530

    Used AuthContext to get reference to state and sigin inside SigninScreen. Also used errorMessage to update errorMessage and also used signin as onSubmite of the AuthForm.

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { Button } from 'react-native-elements';
 import AuthForm from '../components/AuthForm';
 import NavLink from '../components/NavLink';
+import { Context as AuthContext } from '../contexts/AuthContext';
 
 const SigninScreen = ( { navigation } ) => {
+    const { state, signin } = useContext(AuthContext);
+
     return <View style={styles.container}>
         <AuthForm
             headerText="Sign In for tracks"
-            errorMessage=""
+            errorMessage={state.errorMessage}
             buttonText="Sign In"
-            onSubmit={(email, password) => {}}
+            onSubmit={signin}
         />
         <NavLink text="Create an account! Sign Up" routeName='Signup'/>
     </View>
