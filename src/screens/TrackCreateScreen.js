8a75aa29dea138af80d9eb91a089fe65fc2c59de
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-19 18:40:01 +0530

    Added a height to the MapView using styl.

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -6,10 +6,14 @@ import MapView from 'react-native-maps';
 const TrackCreateScreen = () => {
     return <>
     <Text h3>Create Track</Text>
-    <MapView/>
+    <MapView style={styles.map}/>
     </>
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    map: {
+        height: 300
+    }
+});
 
 export default TrackCreateScreen;
