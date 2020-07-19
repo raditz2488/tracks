cf12b71e4321f090277ec921456c13f163d5881a
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-19 18:40:53 +0530

    Imported SafeAreaView from react-navigation.

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import MapView from 'react-native-maps';
+import { SafeAreaView } from 'react-navigation';
 
 const TrackCreateScreen = () => {
     return <>
