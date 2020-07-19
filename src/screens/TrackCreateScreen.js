ede0b523db30e49ad804af63237a88080794cdbb
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-19 18:54:38 +0530

    Added code to calculate random points and also plot them on MapView using Polyline.

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -6,13 +6,11 @@ import { SafeAreaView } from 'react-navigation';
 
 const TrackCreateScreen = () => {
     const points = [];
-    const line = () => {
-        for (let i = 0; i < 20; i++) {
-            points.push({ 
-                latitude: 37.33233 + i * 0.001,
-                longitude: -122.03121 + i * 0.001
-             });
-        }
+    for (let i = 0; i < 20; i++) {
+        points.push({ 
+            latitude: 37.33233 + i * 0.001,
+            longitude: -122.03121 + i * 0.001
+         });
     }
 
     return <>
@@ -26,7 +24,9 @@ const TrackCreateScreen = () => {
                     latitudeDelta: 0.01,
                     longitudeDelta: 0.01
                 }}
-            />
+            >
+                <Polyline coordinates={points}/>
+            </MapView>
         </SafeAreaView>
     </>
 }
