96889e6e94dcfb595ad60dddefd00f00a02344bb
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-19 19:30:18 +0530

    Imported and used Map inside TrackCreateScreen. Alos imported missing components like Polyline and StyleSheet into Map.js.

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -3,38 +3,13 @@ import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import MapView, { Polyline } from 'react-native-maps';
 import { SafeAreaView } from 'react-navigation';
+import Map from '../components/Map';
 
 const TrackCreateScreen = () => {
-    const points = [];
-    for (let i = 0; i < 20; i++) {
-        if (i % 2 == 0) {
-            points.push({ 
-                latitude: 37.33233 + i * 0.001,
-                longitude: -122.03121 + i * 0.001
-             });
-        } else {
-            points.push({ 
-                latitude: 37.33233 - i * 0.001,
-                longitude: -122.03121 + i * 0.001
-             });
-        }
-        
-    }
-
     return <>
         <SafeAreaView>
             <Text h3>Create Track</Text>
-            <MapView 
-                style={styles.map}
-                initialRegion={{
-                    latitude:37.33233,
-                    longitude: -122.03121,
-                    latitudeDelta: 0.01,
-                    longitudeDelta: 0.01
-                }}
-            >
-                <Polyline coordinates={points}/>
-            </MapView>
+            <Map/>
         </SafeAreaView>
     </>
 }
