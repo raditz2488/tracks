bcff16602ceda67dded5f397c4775dcfe705bfa3
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-19 18:06:25 +0530

    Added null to be returned in the AuthResolveScreen component fucntion. Also export default is added.

--- src/screens/AuthResolveScreen.js
+++ src/screens/AuthResolveScreen.js
@@ -1,2 +1,9 @@
 import React, { useEffect, useContext } from 'react';
 import { Context as  AuthContext } from '../contexts/AuthContext';
+import { exp } from 'react-native-reanimated';
+
+const AuthResolveScreen = () => {
+    return null;
+}
+
+export default AuthResolveScreen;
