144665704a6e771d00d9373f3df12c72bc9c0480
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-26 15:34:31 +0530

    Removed code related to calculating random points and showing Polyline on MapView from Map component.

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,22 +3,6 @@ import MapView, { Polyline } from 'react-native-maps';
 import { StyleSheet } from 'react-native';
 
 const Map = () => {
-    const points = [];
-    for (let i = 0; i < 20; i++) {
-        if (i % 2 == 0) {
-            points.push({ 
-                latitude: 37.33233 + i * 0.001,
-                longitude: -122.03121 + i * 0.001
-             });
-        } else {
-            points.push({ 
-                latitude: 37.33233 - i * 0.001,
-                longitude: -122.03121 + i * 0.001
-             });
-        }
-        
-    }
-
     return <MapView 
         style={styles.map}
         initialRegion={{
@@ -28,7 +12,6 @@ const Map = () => {
         longitudeDelta: 0.01 }
         }
     >
-        <Polyline coordinates={points}/>
     </MapView>
 }
 
