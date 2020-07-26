ebd37428d4142f12ae0fa64497d323eda237cbc5
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-26 16:32:54 +0530

    Removed the location fetching logic from TrackCreateScreen and used the useLocation hook instead.

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,40 +1,17 @@
 import '../_mockLocation';
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
 import Map from '../components/Map';
-import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location'; 
 import { Context as LocationContext } from '../contexts/LocationContext';
+import useLocation from '../hooks/useLocation';
 
 const TrackCreateScreen = () => {
-    const [err, setErr] = useState(null);
+    const [err] = useLocation();
 
     const { addLocation } = useContext(LocationContext);
 
-    const startWatching = async () => {
-        const milliSecondsInASecond = 1000
-        const distanceIntervalInMeters = 10
-        try {
-            const granted = await requestPermissionsAsync()
-            if(!granted) {
-                throw new Error("Location permission not granted");
-            }
-            await watchPositionAsync({
-                accuracy: Accuracy.BestForNavigation,
-                timeInterval: milliSecondsInASecond,
-                distanceInterval: distanceIntervalInMeters
-            }, (location) => {
-                addLocation(location);
-            });
-        } catch(e) {
-            setErr(e);
-        }
-    }
-
-    useEffect(() => {
-        startWatching()
-    }, []);
     return <>
         <SafeAreaView>
             <Text h3>Create Track</Text>
