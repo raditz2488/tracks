88aebc0de84403fa6af4f94b33762c572d12587f
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-28 19:46:15 +0530

    Used LocationContext to get access to state, addLocation, startRecording in the TrackForm.js

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
+import { Context as LocationContext } from '../contexts/LocationContext';
 
 const TrackForm = () => {
+
+    const { state, setName, startRecording } = useContext(LocationContext);
+
     return <>
         <Spacer>
             <Input placeholder="Enter track name" />
