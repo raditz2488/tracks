cb7364e1ea2aaaa189bb81eb948ac8e966d3a9dc
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-29 17:37:16 +0530

    Added navigation to TrackList route when saveTrack is complete.

--- src/hooks/useSaveTrack.js
+++ src/hooks/useSaveTrack.js
@@ -1,6 +1,8 @@
 import { useContext } from 'react';
 import { Context as LocationContext } from '../contexts/LocationContext';
 import { Context as TrackContext } from '../contexts/TrackContext';
+import { navigate } from '../navigatorRef';
+
 export default () => {
     const { state: { name, locations }, reset } = useContext(LocationContext);
     const { createTrack } = useContext(TrackContext);
@@ -8,6 +10,7 @@ export default () => {
     const saveTrack = async () => {
         await createTrack(name, locations);
         reset();
+        navigate('TrackList');
     }
 
     return [saveTrack];
