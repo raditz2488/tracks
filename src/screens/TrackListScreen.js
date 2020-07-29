b19ebc1a4b2b81e1df7e34c2c89413a9bde1dce3
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-29 18:04:11 +0530

    Wrapped TrackListScreen around NavigationEvents and called fetchTracks from onWillFocus event.

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -2,18 +2,21 @@ import React, { useContext } from 'react';
 import { StyleSheet, Button, FlatList } from 'react-native';
 import { Context as TrackContext } from '../contexts/TrackContext';
 import { ListItem } from 'react-native-elements';
+import { NavigationEvents } from 'react-navigation';
 
 
 const TrackListScreen = ( { navigation } ) => {
     const { state, fetchTracks } = useContext(TrackContext);
 
     return <>
-        <FlatList 
-            data={state} 
-            keyExtractor={(item) => item._id }
-            renderItem={({ item }) => <ListItem  chevron title={item.name}/>}
-        />
-        <Button title='Show detail' onPress={() => navigation.navigate('TrackDetail')}/>
+        <NavigationEvents onWillFocus={ fetchTracks }>
+            <FlatList 
+                data={state} 
+                keyExtractor={(item) => item._id }
+                renderItem={({ item }) => <ListItem  chevron title={item.name}/>}
+            />
+            <Button title='Show detail' onPress={() => navigation.navigate('TrackDetail')}/>
+        </NavigationEvents>
     </>
 }
 
