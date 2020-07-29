74a5c55efdce199e83e2edb25a6976434fa31772
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-29 14:54:26 +0530

    Added interceptor for request on the axios instance with a failure handler returning reject Promise.

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -4,5 +4,11 @@ const instance =  axios.create({
     baseURL:"http://bf72fd007da3.ngrok.io"
 });
 
+instance.interceptors.request.use(
+    () => {},
+    (err) => {
+        return Promise.reject(err);
+    }
+);
 
 export default instance;
