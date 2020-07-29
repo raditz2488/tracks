08810a033f21278bfd28e3611b02d47d2047af2d
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-29 15:01:57 +0530

    Used AsyncStorage to get access to token and used the token as the Authrization in the header of the config in the tracker axios instance.

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,11 +1,20 @@
 import axios from 'axios';
+import { AsyncStorage } from 'react-native';
 
 const instance =  axios.create({
     baseURL:"http://bf72fd007da3.ngrok.io"
 });
 
 instance.interceptors.request.use(
-    () => {},
+    async (config) => {
+        await AsyncStorage.getItem('token', (err, token) => {
+            if(token) {
+                config.headers.Authorization = `Bearer ${token}`;
+            }
+        });
+
+        return config;
+    },
     (err) => {
         return Promise.reject(err);
     }
