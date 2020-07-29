be6c7c36431b3fe2aa19661f07b74482f710a0f6
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-29 17:39:41 +0530

    Added code to fetch Tracks from api and update TrackContext state by implementing the fetchTracks action.

--- src/contexts/TrackContext.js
+++ src/contexts/TrackContext.js
@@ -3,6 +3,8 @@ import trackerAPI from '../api/tracker';
 
 const trackReducer = (state, action) => {
     switch(action.type) {
+        case 'fetch':
+            return action.payload;
         default: return state;
     }
 }
@@ -10,7 +12,10 @@ const trackReducer = (state, action) => {
 const createTrack = dispatch => async (name, locations) => {
     await trackerAPI.post('/tracks', { name, locations });
 };
-const fetchTracks = dispatch => () => {};
+const fetchTracks = dispatch => async () => {
+    const response = await trackerAPI.get('/tracks');
+    dispatch({ type:'fetch', payload: response.data });
+};
 
 export const { Context, Provider } = createDataContext(
     trackReducer,
