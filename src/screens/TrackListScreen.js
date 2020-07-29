486e9628b92f817891157d3ad183182a2cb109e2
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-29 17:58:40 +0530

    Added keyExtractor and renderItem implementation for FlatList in the TrackListScreen.

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,9 +1,18 @@
 import React, { useContext } from 'react';
 import { StyleSheet, Button, FlatList } from 'react-native';
+import { Context as TrackContext } from '../contexts/TrackContext';
+import { ListItem } from 'react-native-elements';
+
 
 const TrackListScreen = ( { navigation } ) => {
+    const { state } = useContext(TrackContext);
+
     return <>
-        <FlatList data={[]} />
+        <FlatList 
+            data={state} 
+            keyExtractor={(item) => item._id }
+            renderItem={({ item }) => <ListItem  chevron title={item.name}/>}
+        />
         <Button title='Show detail' onPress={() => navigation.navigate('TrackDetail')}/>
     </>
 }
